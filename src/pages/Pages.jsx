668a7f49daa22e643f448d0da730e13e7ac057ae
Guilherme Santana <guihsantana2@gmail.com>
2024-08-20 18:54:03 -0300

    Update Pages.jsx

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -3,11 +3,6 @@ import Home from "../components/MainPage/Home"
 import Descontos from "../components/Descontos/Descontos"
 import Equipamentos from "../components/Equipamentos/Equipamentos"
 import Sementes from "../components/Sementes/Sementes"
-import TopCate from "../components/top/TopCate"
-import NewArrivals from "../components/newarrivals/NewArrivals"
-import Discount from "../components/discount/Discount"
-import Shop from "../components/shops/Shop"
-import Annocument from "../components/annocument/Annocument"
 import Wrapper from "../components/wrapper/Wrapper"
 import Fertilizantes from "../components/Fertilizantes/Fertilizantes"
 import Irrigação from "../components/Irrigação/Irrigação"
@@ -16,22 +11,14 @@ const Pages = ({ productItems, addToCart, CartItem, shopItems }) => {
   return (
     <>
       <Home CartItem={CartItem} />
+
       <Descontos productItems={productItems} addToCart={addToCart} />
       <Equipamentos productItems={productItems} addToCart={addToCart}/>
       <Sementes productItems={productItems} addToCart={addToCart}/>
       <Fertilizantes productItems={productItems} addToCart={addToCart}/>
       <Irrigação productItems={productItems} addToCart={addToCart}/>
 
-      {//<TopCate />
-}
-      {//<NewArrivals />
-}
-      {//<Discount />
-}
-      {//<Shop shopItems={shopItems} addToCart={addToCart} />
-}
-      {//<Annocument />
-      }
+      
       <Wrapper />
     </>
   )
